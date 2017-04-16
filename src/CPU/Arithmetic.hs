4c0d7ecefe998cda3952935657a3c23eb1c01387
Paul Courts <paul.courts@autodesk.com>
2017-04-16 19:04:24 +0100

    Added some instructions until something weird happened

--- src/CPU/Arithmetic.hs
+++ src/CPU/Arithmetic.hs
@@ -2,6 +2,7 @@
 
 module CPU.Arithmetic (
     carriedAdd
+  , carriedSubtract
     ) where
 
 import Data.Word
@@ -11,13 +12,16 @@ type Carry = Bool
 as32 :: (Integral a) => a -> Word32 
 as32 = fromIntegral
 
-
 carriedAdd :: forall w. (Integral w, Bounded w) => w -> w -> (w, Carry)
 carriedAdd x y = 
     let 
         max = as32 (maxBound :: w)
         realAnswer = (as32 x) + (as32 y)
     in 
-        (fromIntegral realAnswer :: w, realAnswer > max)
+        (fromIntegral realAnswer :: w, realAnswer > max) 
 
-
+-- I believe what happens is the (2's complement) negative of y
+-- is added to x, and the flags set accordingly.  
+carriedSubtract :: (Integral w, Bounded w) => w -> w -> (w, Carry)
+carriedSubtract x y = 
+    carriedAdd x (-y)
--- src/CPU/Instructions.hs
+++ src/CPU/Instructions.hs
@@ -7,6 +7,7 @@ import BitTwiddling
 import CPU.Types
 import CPU.Environment
 import CPU.Flags
+import CPU.Arithmetic
 import CPU
 import ShowHex
 
@@ -25,12 +26,21 @@ execute op = case op of
     0x01 -> ld (directToCombo BC) =<< fetch16  -- LD BC,d16
     0x05 -> dec b                              -- DEC B
     0x06 -> ld (direct b) =<< fetch            -- LD B,d8
+    0x0D -> dec c                              -- DEC C
     0x0E -> ld (direct c) =<< fetch            -- LD C,d8
     0x20 -> jr NZc =<< fetch                   -- JR NZ,r8
     0x21 -> ld (directToCombo HL) =<< fetch16  -- LD HL,d16
+    0x31 -> ld (direct16 sp) =<< fetch16       -- LD SP,d16
     0x32 -> ld (derefMinus HL) =<< readReg a   -- LD (HL-),A 
+    0x3E -> ld (direct a) =<< fetch            -- LD A,d8
+    0x36 -> (ld (deref HL) =<< fetch) >> return 12    -- LD (HL),d8
     0xAF -> xor =<< readReg a                  -- XOR A
-    0xC3 -> jp  =<< fetch16                    -- JP d16
+    0xC3 -> jp =<< fetch16                     -- JP d16
+    0xE0 -> ldh_a8_a                           -- LDH (a8),A
+    0xEA -> ld derefImmediate =<< readReg a    -- LD (a16),A 
+    0xF0 -> ldh_a_a8                           -- LDH A,(a8)
+    0xFE -> cp =<< fetch                       -- CP d8
+    0xF3 -> di                                 -- DI
     _    -> error $ "Unknown opcode " ++ (showHex op)
 
 
@@ -47,14 +57,14 @@ condition f = case f of
     Any -> return True
 
 -- Functions for loading to different destinations
-direct :: CPURegister s Word8 -> (Word8 -> CPU s Cycles)
-direct reg = \w ->
+direct :: CPURegister s Word8 -> Word8 -> CPU s Cycles
+direct reg w =
     writeReg reg w >>
     return 8
 
-directToSP :: Word16 -> CPU s Cycles
-directToSP w =
-    writeReg sp w >>
+direct16 :: CPURegister s Word16 -> (Word16 -> CPU s Cycles)
+direct16 reg w = 
+    writeReg reg w >>
     return 12
 
 directToCombo :: ComboRegister -> (Word16 -> CPU s Cycles)
@@ -65,13 +75,24 @@ directToCombo reg = \w ->
 -- e.g. LD (HL-) instructions.
 -- Dereference the register, write the value to the address,
 -- then decrement the register.
-derefMinus :: ComboRegister -> (Word8 -> CPU s Cycles)
-derefMinus reg = \w -> do
+derefMinus :: ComboRegister -> Word8 -> CPU s Cycles
+derefMinus reg w = do
     addr <- readComboReg reg
     writeMemory addr w
     writeComboReg reg (addr - 1)
     return 8
 
+deref :: ComboRegister -> Word8 -> CPU s Cycles
+deref reg w = do
+    addr <- readComboReg reg
+    writeMemory addr w
+    return 8
+
+derefImmediate :: Word8 -> CPU s Cycles
+derefImmediate w = do
+    addr <- fetch16
+    writeMemory addr w
+    return 16
 
 -- NOP: Blissfully let life pass you by.
 nop :: CPU s Cycles
@@ -97,6 +118,28 @@ xor byte = do
 ld :: (w -> CPU s Cycles) -> w -> CPU s Cycles
 ld = id
 
+-- LDH (a8),A: 
+-- Load the contents of A into address (a8 + 0xFF00)
+-- Cheated a bit in defining this. It should take a destination function like the others.
+ldh_a8_a :: CPU s Cycles
+ldh_a8_a = do
+    a8 <- fetch
+    let addr = 0xFF00 + (fromIntegral a8 :: Word16)
+    a <- readReg a
+    writeMemory addr a
+    return 12
+
+-- LDH A,(a8): 
+-- Load the contents of (a8 + 0xFF00) into A
+-- Cheated a bit in defining this. It should take a destination function like the others.
+ldh_a_a8 :: CPU s Cycles
+ldh_a_a8 = do
+    a8 <- fetch
+    let addr = 0xFF00 + (fromIntegral a8 :: Word16)
+    v <- readMemory addr
+    writeReg a v
+    return 12
+
 -- DEC: Decrease a register or memory location by 1
 -- Currently just the 8-bit registers.
 -- Need to rethink this when I get to the others.
@@ -107,6 +150,16 @@ dec reg = do
     v <- readReg reg
     setFlags (As (v == 0), On, Off, NA) -- half-carry is complicated, gonna ignore it right now
     return 4
+
+-- CP: Compare with A to set flags.
+-- Flags: Z 1 H C
+cp :: Word8 -> CPU s Cycles
+cp byte = do
+    a <- readReg a
+    let (result, c) = carriedSubtract a byte 
+    setFlags (As (result == 0), On, Off, As c)
+    return 8
+
 
 -- JR: Relative conditional jump
 -- cc is a flag condition. 
@@ -126,8 +179,6 @@ jr cc byte = do
             
     where signed = fromIntegral $ toSigned byte
 
-
-
-
-
-
+-- DI: Disable interrupts
+di :: CPU s Cycles
+di = return 4 -- Haven't implemented interrupts yet.
